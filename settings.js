ba93173a2e54610f0e9b62629732a746e4c39ee5
Vosty17 <stephwafula70@gmail.com>
2025-08-20 20:04:18 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'snowbird',
   botName: "Lady Bella",
   botOwner: 'Snowbird', // Your name
-  ownerNumber: '263780145644', //Set your number here without + symbol.
+  ownerNumber: '254117812436', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
